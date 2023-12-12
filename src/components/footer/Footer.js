ed5f316f9eab7a0a5b3db814fcdab80e4afbed15
github_christiandc19 <christiandc19@hotmail.com>
2023-12-11 20:57:44 -0800

    styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
-
-import logo from "../../assets/nav-logo.png";
-import DHCS from "../../assets/DHCS-logo.png";
-
+import logo from '../../assets/nav-logo.png'
 import "./FooterStyles.css";
+import { MdPhoneCallback } from "react-icons/md";
+import { MdAlternateEmail } from "react-icons/md";
+import { FiMapPin } from "react-icons/fi";
+
 
 import { Link as LinkRoll } from "react-scroll";
 
@@ -16,163 +17,176 @@ const Footer = () => {
         <div className="container">
           <div className="top">
             <div className="logo-footer">
-              <img src={logo} alt="Santa Monica Rehab Logo" loading="lazy" />
+              <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                <Link to='/'>
+                  <img src={logo} alt="Oxnard Rehab Logo" loading="lazy"/>
+                </Link>
+              </LinkRoll>
             </div>
 
-            <LinkRoll
-              activeClass="active"
-              to="top"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <BsFillArrowUpCircleFill className="footer-icon2" />
-            </LinkRoll>
 
-            <LinkRoll
-              activeClass="active"
-              to="top"
-              spy={true}
-              smooth={true}
-              duration={500}
-            >
-              <BsFillArrowUpCircleFill className="footer-icon" />
-            </LinkRoll>
+            <div>
+              <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                <BsFillArrowUpCircleFill className="icon" />
+              </LinkRoll>
+            </div>
+
+
+
           </div>
+
+
+
           <div className="col-container">
-            <div className="footer-col">
-              <h3>Company</h3>
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/mission">
-                  <p>Our Mission</p>
-                </Link>
-              </LinkRoll>
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/about-us">
-                  <p>About Us</p>
-                </Link>
-              </LinkRoll>
+{/* NAVIGATION */}
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/team">
-                  <p>Our Team</p>
-                </Link>
-              </LinkRoll>
-
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/testimonials">
-                  <p>Testimonials</p>
-                </Link>
-              </LinkRoll>
-            </div>
-
-            <div className="footer-col">
+            <div className="col">
               <h3>Navigation</h3>
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/">
-                  <p>Home</p>
-                </Link>
-              </LinkRoll>
+{/* HOME */}
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/mental-health">
-                  <p>Mental Health Treatment</p>
-                </Link>
-              </LinkRoll>
+              <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/">
+                    <p>Home</p>
+                  </Link>
+                </LinkRoll>
+              </div>
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/substance-abuse">
-                  <p>Substance Abuse Treatment</p>
-                </Link>
-              </LinkRoll>
+{/* JOB */}
 
-              <LinkRoll
-                activeClass="active"
-                to="top"
-                spy={true}
-                smooth={true}
-                duration={500}
-              >
-                <Link to="/jobs">
-                  <p>Jobs Program</p>
-                </Link>
-              </LinkRoll>
+              <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/insurance">
+                    <p>Insurance</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+
+{/* CONTACT */}
+
+            <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/contact">
+                    <p>Call Us</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
             </div>
 
-            <div className="license">
-              <h3>Licensed by the State Department of Health Care Services</h3>
-              <p>
-                License Number: 191057BP <br />
-                Expiration Date: 6 / 30 / 2025{" "}
-              </p>
-              <br />
 
-              <a
-                href="https://data.chhs.ca.gov/dataset/sud-recovery-treatment-facilities"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ( View License & Certification Information here ).
-              </a>
+
+            <div className="col">
+              <h3>Company</h3>
+
+{/* DETOX */}
+              <div className="hide">
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/mission">
+                    <p>Our Mission</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+{/* RESIDENTIAL */}
+
               <div>
-                <img
-                  src={DHCS}
-                  width="300px"
-                  height="150px"
-                  alt="Santa Monica Rehab Logo"
-                  loading="lazy"
-                />
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/team">
+                    <p>Our Team</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+{/* PHP */}
+
+            <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/testimonials">
+                    <p>Testimonials</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+            </div>
+
+
+
+
+
+
+
+            <div className="col">
+              <h3>Treatment Programs</h3>
+
+{/* PHP */}
+              <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/mental-health">
+                    <p>Mental Health</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+{/* IOP */}
+
+              <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/substance-abuse">
+                    <p>Substance Abuse Programs</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+{/* OP */}
+
+              <div>
+                <LinkRoll activeClass="active" to="top" spy={true} smooth={true} duration={500}>
+                  <Link to="/jobs">
+                    <p>Jobs Program</p>
+                  </Link>
+                </LinkRoll>
+              </div>
+
+            </div>
+
+
+
+
+
+
+{/* Contact */}
+            <div className="col">
+              <h3>CONTACT</h3>
+              <div>
+                  <p><span><MdPhoneCallback /></span> <a href="tel:1234567890">(213) 584-4445</a></p>
+              </div>
+
+              <div>
+                  <p><span><MdAlternateEmail  /><a href = "mailto: abc@example.com">info@santamonicarehab.biz</a></span></p>
+              </div>
+
+              <div>
+                  <p><span><FiMapPin  /></span> Santa Monica, California</p>
               </div>
             </div>
           </div>
 
+
+
+
+
+
+
+
           <p className="copyright">
-            <p>&copy; 2023 Santa Monica Rehab. All rights reserved</p>
+            <p>
+              &copy; 2023 Santa Monica Rehab <br/> All rights
+              reserved
+            </p>
           </p>
         </div>
       </div>
